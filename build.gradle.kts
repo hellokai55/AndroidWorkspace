b5f628bc11e1ad3594c43c34b2b5ee7884e16b72
pengkai <pengkai@pengkaideMacBook-Pro.local>
2023-08-15 22:02:30 +0800

    enable versionCatalogs

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(deps.plugins.android.application) apply false
+    alias(deps.plugins.android.library) apply false
+    alias(deps.plugins.kotlinAndroid) apply false
+}
