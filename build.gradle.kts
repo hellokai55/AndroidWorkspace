d18d0dd092dd8769d90dcca2f3d5e915f6c47011
hellokai <1073556549@qq.com>
2023-09-25 15:18:42 +0800

    修改gradle到8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(deps.plugins.android.application) apply false
-    alias(deps.plugins.android.library) apply false
-    alias(deps.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
 }
+true // Needed to make the Suppress annotation work for the plugins block
