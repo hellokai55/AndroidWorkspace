e25ce7896bc43636f0ddba2f51bd1e1fdffd40b3
hellokai55 <1073556549@qq.com>
2024-01-05 14:00:07 +0800

    feat: 添加修改AndroidManifest.xml的能力

--- buildSrc/src/main/kotlin/com/hellokai/androidworkspace/buildsrc/VariantV2AdvancedPlugin.kt
+++ buildSrc/src/main/kotlin/com/hellokai/androidworkspace/buildsrc/VariantV2AdvancedPlugin.kt
@@ -12,6 +12,7 @@ import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
@@ -38,11 +39,34 @@ class VariantV2AdvancedPlugin : Plugin<Project> {
             }
             val variantCapitalizedName = variant.name.capitalized()
 
+            // 修改APK位置
             val renameApkTask = project.tasks.register<RenameApkFile>("renameV2${variantCapitalizedName}Apk") {
                 val apkFolderProvider = variant.artifacts.get(SingleArtifact.APK)
                 this.outApk.set(File(apkFolderProvider.get().asFile, "custom-name-${variant.name}.apk"))
                 this.apkFolder.set(apkFolderProvider)
                 this.builtArtifactsLoader.set(variant.artifacts.getBuiltArtifactsLoader())
+            }
+
+            // 修改AndroidManifest.xml
+            val postUpdateTask = project.tasks.register<ManifestAfterMergeTask>(
+                "postUpdate${variantCapitalizedName}Manifest"
+            )
+            variant.artifacts
+                .use(postUpdateTask)
+                .wiredWithFiles(
+                    ManifestAfterMergeTask::mergedManifest,
+                    ManifestAfterMergeTask::updatedManifest,
+                )
+                .toTransform(SingleArtifact.MERGED_MANIFEST)
+
+            val getManifestTask = project.tasks.register<GetManifestTask>(
+                "getManifest${variantCapitalizedName}"
+            ) {
+                updatedManifest.set(variant.artifacts.get(SingleArtifact.MERGED_MANIFEST))
+            }
+
+            renameApkTask.configure {
+                dependsOn(getManifestTask)
             }
         }
     }
@@ -63,6 +87,29 @@ class VariantV2AdvancedPlugin : Plugin<Project> {
                 ?: throw RuntimeException("Cannot load APKs")
             File(builtArtifacts.elements.single().outputFile)
                 .copyTo(outApk.get().asFile)
+        }
+    }
+
+    abstract class ManifestAfterMergeTask: DefaultTask() {
+        @get:InputFile
+        abstract val mergedManifest: RegularFileProperty
+        @get:OutputFile
+        abstract val updatedManifest: RegularFileProperty
+        @TaskAction
+        fun afterMerge() {
+            val modifiedManifest = mergedManifest.get().asFile.readText()
+                .replace("allowBackup=\"true\"", "allowBackup=\"false\"")
+            updatedManifest.get().asFile.writeText(modifiedManifest)
+        }
+    }
+
+    abstract class GetManifestTask: DefaultTask() {
+        @get:InputFile
+        abstract val updatedManifest: RegularFileProperty
+
+        @TaskAction
+        fun afterMerge() {
+            println(updatedManifest.get().asFile.readText())
         }
     }
 
